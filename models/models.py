defba35405865527eb3c0a4d7abee96a08d693ad
Levis Libra <levislibra@hotmail.com>
2017-09-14 11:49:07 -0300

    update ok

--- models/models.py
+++ models/models.py
@@ -7,20 +7,27 @@ class CtaCte(models.Model):
 	_inherit = 'res.partner'
 
 	def ver_ctacte(self, cr, uid, ids, context=None):
+		partner_obj = self.pool.get('res.partner')
+		partner_id = partner_obj.browse(cr, uid, ids[0], context=None)
+
+		property_account_receivable_id = partner_id.property_account_receivable_id.id
+		property_account_payable_id = partner_id.property_account_payable_id.id
+
 		ctacte_obj = self.pool.get('account.move.line')
-		ctacte_ids = ctacte_obj.search(cr, uid, [])
+		ctacte_ids = ctacte_obj.search(cr, uid, [
+			('partner_id', '=', partner_id.id),
+			('account_id', 'in', [property_account_receivable_id, property_account_payable_id]),
+			])
 
 		model_obj = self.pool.get('ir.model.data')
 		data_id = model_obj._get_id(cr, uid, 'l10n_ar_cta_cte', 'view_ctacte')
-		print data_id
 		view_id = model_obj.browse(cr, uid, data_id, context=None).res_id
-		print view_id
 		return {
 			'domain': "[('id', 'in', ["+','.join(map(str, ctacte_ids))+"])]",
 			'name': ('Cta Cte'),
 			'view_type': 'form',
 			'view_mode': 'tree',
 			'res_model': 'account.move.line',
-			'view_id': 12802,
+			'view_id': view_id,
 			'type': 'ir.actions.act_window',
 		}
